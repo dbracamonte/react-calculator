ad512de7148652b9d33557bdeccd57b925891d4b
Darwin Bracamonte <bracamontedar@gmail.com>
2018-10-18 22:24:17 -0400

    Modificado las keys del componente Numbers

--- src/calculator/components/Numbers/index.js
+++ src/calculator/components/Numbers/index.js
@@ -5,16 +5,16 @@ const numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "."];
 
 const Numbers = props => (
   <div className="Numbers">
-    {numbers.map(item =>
+    {numbers.map((item, index) =>
       <button
         className="Btn-number"
-        key={item}
+        key={`n-` + index}
         value={item}
         onClick={props.handleClick}>
         {item}
       </button>
     )}
-  </div> 
+  </div>
 )
 
 export default Numbers;
